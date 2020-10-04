9218cfd51d5d745cb9bd233631f148b11934d83e
Ivan <gramprogramming@gmail.com>
2020-10-04 21:44:35 +0500

    Create routing
    Create new pages
    Create routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import IndexPage from "../index-page/index-page";
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import IndexPage from "../index-page/index-page";
+import LoginPage from "../login-page/login-page";
+import FavoritePage from "../favorites-page/favorites-page";
+import OfferPage from "../offer-page/offer-page";
 
 const App = (props) => {
   const {offersQuantity} = props;
 
   return (
-    <IndexPage offersQuantity={offersQuantity} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <IndexPage offersQuantity={offersQuantity} />
+        </Route>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritePage />
+        </Route>
+        <Route exact path="/offer/:id">
+          <OfferPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
