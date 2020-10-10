d3176a72cd03723d2dcbf6f5b41834a599d7c5a4
Ivan Krivilev <gramprogramming@gmail.com>
2020-10-10 17:37:32 +0500

    Сonnection mocks: offers, reviews for app

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
 
+import offers from "./mocks/offers";
+import reviews from "./mocks/reviews";
+
 const Settings = {
   OFFERS_QUANTITY: 312
 };
 
-ReactDOM.render(<App offersQuantity={Settings.OFFERS_QUANTITY} />, document.querySelector(`#root`));
+ReactDOM.render(
+    <App
+      offersQuantity={Settings.OFFERS_QUANTITY}
+      offers={offers}
+      reviews={reviews}
+    />,
+    document.querySelector(`#root`)
+);
